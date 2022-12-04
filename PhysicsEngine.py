d73fcc09348076e915c63072aeb82785e3dc25df
EmilioMendoza <Ironislife39@gmail.com>
2022-12-04 11:01:22 -0500

    Broken Collsion Detection

--- PhysicsEngine.py
+++ PhysicsEngine.py
@@ -1,33 +1,39 @@
+from typing import Optional
+
 from pygame import sprite
+
 
 class PhysicsEngine:
     def __init__(self):
         self.distance = 0
-        self.overrideDistance = False
 
-    def earthGravity(self, smallmass: float, largemass_y: int, smallmass_y: int) -> float:
-        """Returns a float containing the change in value needed to simulate gravity"""
-        if not self.overrideDistance:
+    def earthGravity(self, smallmass: float, largemass_y: float, smallmass_y: float, distance=None) -> float:
+        """Returns a float containing the change in y-value needed to simulate gravity"""
+        if not distance:
             self.distance = (6.4 * (10 ** 6)) ** 2
+        else:
+            self.distance = distance
         try:
             force = ((6.67 * (10 ** -11)) * (6 * 10 ** 24) * smallmass) / self.distance
         except ZeroDivisionError:
             force = 0
         return force
 
-    def verticalCollision(self, obj1, collisionobjs: list[any], dy: float, top_collision=None, bottom_collision=None) -> list[any]:
-        """Returns a list of objects that a object has collided with and runs function on collision if given one"""
+    def verticalCollision(self, obj1, collisionobjs: list[any], dy: float) -> list[any]:
+        """Returns a list of objects that a object has collided with, the index of the, and the type of collision
+            Assumes all objects have values for object.vector, object.rect, object.height"""
         collided_objects = []
         for obj in collisionobjs:
             if sprite.collide_mask(obj1, obj):
                 if dy > 0:
+                    # Player hit floor
                     obj1.rect.bottom = obj.rect.top
-                    if bottom_collision != None:
-                        bottom_collision()
                 elif dy < 0:
+                    # Player hit roof
                     obj1.rect.top = obj.rect.bottom
-                    if top_collision != None:
-                            top_collision()
                 collided_objects.append(obj)
-        
+        """for obj in collisionobjs:
+            if obj1.rect.colliderect(obj.rect):
+                obj1.rect.bottom = obj.rect.top
+                collided_objects.append(obj)"""
         return collided_objects
