15965b3d3509a143fcc8b0f4beb5059c53470317
Emilio Mendoza <Ironislife39@gmail.com>
2022-12-08 08:43:22 -0500

    Added collide funcs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import SmallObject
 import time
 import Physics2D
+import Collision2D
 
 pygame.init()
 
@@ -26,10 +27,19 @@ while run:
     obj2.draw(win)
     obj1.draw(win)
 
-
-    pygame.draw.rect(win, (0, 255, 0), pygame.Rect(obj1.bottom.x, obj1.bottom.y, 10, 10))
-    movetowards = Physics2D.update(obj1.rb.mass, obj2.rb.mass, pygame.math.Vector2(obj1.vector.x, obj1.vector.y), obj2.vector)
+    collide = Collision2D.detectCollision(obj1, [obj2])
+    if collide[0] and obj1.jumping:
+        obj1.rect.bottom = collide[1].rect.top
+        obj1.vector.y = obj1.rect.top
+        obj1.jumping = False
+        print("colllide")
+        #obj1.vector = pygame.math.Vector2(10, 100)
+    elif not collide[0] and obj1.jumping:
+        movetowards = Physics2D.update(obj1.rb.mass, obj2.rb.mass, pygame.math.Vector2(obj1.vector.x, obj1.vector.y), obj2.vector)
+        obj1.vector.y = movetowards.y
+        #print("move down")
+    pygame.draw.rect(win, (0, 255, 0), pygame.Rect(obj1.bottom.x, obj1.bottom.y, 10, 10))   
     #print(movetowards)
-    obj1.vector = movetowards
-    print(obj1.vector, obj2.vector)
+    
+    #print(obj1.vector, obj2.vector)
     pygame.display.update()
